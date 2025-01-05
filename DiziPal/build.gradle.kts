8941ed54873f62cf0a26b33f308cc67a75cc3f24
bulucmuluc <129214586+bulucmuluc@users.noreply.github.com>
2025-01-05 20:09:38 +0300

    Update build.gradle.kts

--- DiziPal/build.gradle.kts
+++ DiziPal/build.gradle.kts
@@ -1,4 +1,4 @@
-version = 20
+version = 21
 
 cloudstream {
     authors     = listOf("keyiflerolsun", "muratcesmecioglu")
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("TvSeries", "Movie")
-    iconUrl = "https://www.google.com/s2/favicons?domain=https://dizipal683.com&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=https://dizipal844.com&sz=%size%"
 }
